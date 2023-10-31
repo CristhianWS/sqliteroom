e70112545e53ef4ba221d85752daab290d6d70c2
Cristhian <cristhiansilvaalves@gmail.com>
2023-10-30 21:46:05 -0300

    Att invetory edit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,10 @@ android {
 
 dependencies {
     // Import the Compose BOM
+    //Room
+    implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
+    ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
+    implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
     implementation(platform("androidx.compose:compose-bom:2023.06.01"))
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.compose.material3:material3")
@@ -80,6 +84,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
     implementation("androidx.navigation:navigation-compose:2.6.0")
+
 
     //Room
     implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
